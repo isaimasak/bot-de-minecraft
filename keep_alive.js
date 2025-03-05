8d275889c156cda2b28adb5ab16c0e69e87645ff
isaimasak <96793993+isaimasak@users.noreply.github.com>
2025-03-04 20:09:30 -0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Estoy activo');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 const https = require('https');
